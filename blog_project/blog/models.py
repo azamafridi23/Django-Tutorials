8e2a3a49ed4f16d5173e97584681d504c7b76597
Azam <iamazamafridi@gmail.com>
2023-07-19 11:41:23 +0500

    Learnt database and migrations in django

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,22 @@
+'''
+In Django, the models.py file is used to define the database structure and behavior of your application. 
+It is a Python module that contains the Django model classes, which represent database tables.
+
+'''
+
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model): # Each class will be its own table in the database
+    title = models.CharField(max_length=100) # These are my fields
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
