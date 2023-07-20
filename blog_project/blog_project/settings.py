b6bf15ac717a7557e147410f9a98741c40906ddb
Azam <iamazamafridi@gmail.com>
2023-07-20 13:46:26 +0500

    Learnt and implemented the login and logout feature for my blog page

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
@@ -125,3 +125,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
+
+
