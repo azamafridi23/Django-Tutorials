136920dada844b65c58f270a664a495e7ae0c9e0
Azam <iamazamafridi@gmail.com>
2023-07-25 11:49:08 +0500

    Learned and implemented <User updating profile> functionality

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media') # WHERE UPLOADED FILES WILL BE SAME
+
+MEDIA_URL = "/media/" # How to access the image/file in the browser
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -131,5 +136,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+
 
 
